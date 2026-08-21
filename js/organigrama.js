c53e05acb35d50437a4b426d1fe1434a649d8388
AnntoBarbero <antyy_96@hotmail.com>
2026-08-21 18:03:50 -0300

    Actualizar la ruta de la imagen de César Verna en el organigrama

--- js/organigrama.js
+++ js/organigrama.js
@@ -47,7 +47,7 @@ const personasOrganigrama = {
         cargo: "Director",
         zona: "Zona 1",
         asociacion: "Alta Italia",
-        foto: "../img/organigrama/cesar_verna.png"
+        foto: "../img/organigrama/Cesar_Verna.png"
     },
 
     "ignacio-chiurazzi": {
